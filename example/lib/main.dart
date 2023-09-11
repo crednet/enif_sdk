46bdb2c7f655e7c8cdcf517754f833a8ba49b68c
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2023-09-11 14:38:56 +0100

    updated enif controller

---
+++ example/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:enif/bloc/get_user_chats_bloc.dart';
+import 'package:enif/bloc/new_chat_bloc.dart';
+import 'package:enif/bloc/send_chat_bloc.dart';
+import 'package:enif/enif.dart';
+import 'package:enif/models/enif_user_params.dart';
+import 'package:enif/modules/home/views/enif_home.dart';
+import 'package:enif/service/api_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+// AppPreferences? appPreferences;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  EnifController.setBusinessId(
+      "438b9f5c-cb40-4861-95d0-0fcdc24ccb624f518a56-964d-45f1-bc68-ba1191d62b78");
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  EnifController.setUser(EnifUserParams(
+      firstName: "Sade",
+      lastName: "Oluchi",
+      phoneNo: "0803000000",
+      email: "sade@enif.com"));
+  // appPreferences = await AppPreferences.getInstance();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider<GetUserChatBloc>(
+            create: (context) => GetUserChatBloc(ApiProvider()),
+          ),
+          BlocProvider<SendChatBloc>(
+            create: (context) => SendChatBloc(ApiProvider()),
+          ),
+          BlocProvider<NewChatBloc>(
+            create: (context) => NewChatBloc(ApiProvider()),
+          ),
+          // BlocProvider<GetFaqBloc>(
+          //   create: (context) => GetFaqBloc(ApiProvider()),
+          // ),
+        ],
+        // child: ScreenUtilInit(
+        //     designSize: const Size(360, 690),
+        //     minTextAdapt: true,
+        //     splitScreenMode: true,
+        //     builder: (context, child) {
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Enif Chat Bot',
+          home: const EnifHome(),
+          builder: EasyLoading.init(), /*  builder: EasyLoading.init()*/
+        ));
+    // }),
+    // );
+  }
+}

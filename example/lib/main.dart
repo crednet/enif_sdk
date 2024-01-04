0cc4d649f10605fe34d0e42376651d13a9303f19
otitibeth <eigbelizabeth@gmail.com>
2024-01-04 10:59:46 +0100

    EnifController.setUser reverted

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,14 +10,14 @@ void main() async {
      "3421a016-ef8b-4c1d-9ced-34147e11e158bfc90848-721c-40d4-b535-b1ef7054f145",
       Env.dev);
 
-  // EnifController.setUser(
-    // EnifUserParams(
-      // firstName: "Sade",
-      // lastName: "Oluchi",
-      // phoneNo: "0803000000",
-      // email: "sade@enif.com"
-      // )
-      // );
+  EnifController.setUser(
+    EnifUserParams(
+      firstName: "Sade",
+      lastName: "Oluchi",
+      phoneNo: "0803000000",
+      email: "sade@enif.com"
+      )
+      );
   
   EnifController.setDeviceToken(
       "cft5xLfER0i4PeRZEupqqp:APA91bFmWW0LuTYF7dbAm7bxEMUGkUWE8hKilLA6HTdFHu-vCTQeYCRunXkPJBnzLk3Bh27VRGCIkM3M2U7SmQHsbkK7W3Kns4kQSoG6ZhSQHU2ihjcpFSASBxeoZ3Ff6cJSGu4FIsRC"
